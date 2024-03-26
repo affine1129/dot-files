93e226831214e93198b333114dc4eae5fe1fa60e
affine1129 <affine1129@gmail.com>
2024-03-26 10:40:32 +0900

    Update installation apps

--- setup.sh
+++ setup.sh
@@ -1,15 +1,38 @@
+# CLIs
+
 # https://github.com/anyenv/anyenv
 brew install anyenv
-
 # https://github.com/pyenv/pyenv-virtualenv
 brew install pyenv-virtualenv
-
 # https://github.com/BurntSushi/ripgrep
 brew install ripgrep
-
 # https://github.com/sharkdp/fd
 brew install fd
+# https://github.com/abiosoft/colima
+brew install colima
+# https://github.com/jesseduffield/lazydocker
+brew install lazydocker
+# https://github.com/jesseduffield/lazygit 
+brew install lazygit
+# https://github.com/docker/cli
+brew install docker
+# https://github.com/neovim/neovim?tab=readme-ov-file
+brew install neovim
 
+brew install git
+
+
+# Apps
+
+# https://wezfurlong.org/wezterm/install/macos.html 
 brew install --cask wezterm
+# https://github.com/pqrs-org/Karabiner-Elements
+brew install --cask karabiner-elements
+
+brew install --cask arc
+brew install --cask raycast
+
+
+# configuration
 
 sh ./sh/link.sh
