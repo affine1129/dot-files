71f8cef5739e7d1416893ee91f9f1d59bcdebda3
affine1129 <affine1129@gmail.com>
2025-08-06 12:16:48 +0900

    :sparkles: Add go fmt setting

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,6 +4,21 @@ return {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim'
   },
+  on_attach = function(client, bufnr)
+    if vim.lsp.server_capabilities.documentHighlightProvider then
+      vim.cmd [[
+        set updatetime=500
+        highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+        highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+        highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+        augroup lsp_document_highlight
+          autocmd!
+          autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+          autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+        augroup END
+      ]]
+    end
+  end,
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup({
@@ -46,16 +61,27 @@ return {
       end,
     })
 
-    vim.cmd [[
-      set updatetime=500
-      highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-      highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-      highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
-      augroup lsp_document_highlight
-        autocmd!
-        autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-      augroup END
-    ]]
+    vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+      local params = vim.lsp.util.make_range_params()
+      params.context = {only = {"source.organizeImports"}}
+      -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+      -- machine and codebase, you may want longer. Add an additional
+      -- argument after params if you find that you have to write the file
+      -- twice for changes to be saved.
+      -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+      for cid, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+          if r.edit then
+            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+          end
+        end
+      end
+      vim.lsp.buf.format({async = false})
+    end
+  })
   end
 }
